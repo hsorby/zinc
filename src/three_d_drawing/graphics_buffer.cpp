a97b1be080cc2fb9fb85797333b3f0211a8cc5a9
hsorby <h.sorby@auckland.ac.nz>
2022-04-03 20:55:36 +1200

    Add debug marker.

--- src/three_d_drawing/graphics_buffer.cpp
+++ src/three_d_drawing/graphics_buffer.cpp
@@ -722,6 +722,7 @@ void Graphics_buffer_blit_framebuffer(struct Graphics_buffer *buffer)
 
 int Graphics_buffer_set_multisample_framebuffer(struct Graphics_buffer *buffer, int preferred_antialias)
 {
+    printf("helllooooooo");
 #ifdef GL_EXT_framebuffer_multisample
 
 	 int antialias;
